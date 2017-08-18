44e9dffe79d41939a2cfad25bfaef4ba43087351
Yana Wilkins <yav9774@gmail.com>
2017-08-18 11:01:38 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -4,42 +4,28 @@ namespace MathOperations
 {
     class Program
     {
-        public static double Sum (double x, double y)
+        public static void showResults(string number1, string number2)
         {
-            return x + y; 
-        }
-        public static double Difference (double x, double y)
-        {
-            return x - y; 
-        }
-        public static double Multiplication (double x, double y)
-        {
-            return x * y; 
-        }
-        public static double Division (double x, double y)
-        {
-             return x / y; 
-        }
-        
-
-        public static void showResults(string number1, string number2){
-        double operand1 = double.Parse(number1);
-        double operand2 = double.Parse(number2);
-        double sum = Sum(operand1, operand2);
-        double difference = operand1 - operand2;
-        double division = operand1 / operand2;
-        double multiplication = operand1 * operand2;
-        var isEven = false;
-
-
-
+            double operand1 = double.Parse(number1);
+            double operand2 = double.Parse(number2);
+            double sum = operand1 + operand2;
+            double difference = operand1 - operand2;
+            double division = operand1 / operand2;
+            double multiplication = operand1 * operand2;
+            var isEven = false;
+            if ((operand1 % operand2) == 0)
+            {
+                isEven = true;
+            }
         }
 
         static void Main(string[] args)
         {
-          
-        String number1 = Console.ReadLine();
-        String number2 = Console.ReadLine();
+
+            String number1 = Console.ReadLine();
+            String number2 = Console.ReadLine();
+
+            showResults(number1, number2);
         }
     }
 }
